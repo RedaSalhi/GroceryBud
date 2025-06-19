f49a3cfeaad4d5216217692d3a8596683e4b5613
RedaSalhi <salhi.reda47@gmail.com>
2025-06-19 17:05:52 +0100

    Add auth helpers and tests

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,7 +4,8 @@ import uuid from 'react-native-uuid';
  * Generate a RFC4122 UUID string (uses 'react-native-uuid' for cross-platform support)
  */
 export const generateUUID = () => {
-  return uuid.v4();
+  const lib = uuid && typeof uuid.v4 === 'function' ? uuid : uuid.default;
+  return lib.v4();
 };
 
 export default { generateUUID };
